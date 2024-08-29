068546920100416ceafc1bcd22f8decff7f52a5d
proyecto-clinica <isai123abel@gmail.com>
2024-08-29 09:18:10 -0600

    ultimos cambios

--- marketing/controllers/controllerHeatmap/heatmapController.js
+++ marketing/controllers/controllerHeatmap/heatmapController.js
@@ -4,12 +4,17 @@ const Heatmap = require('../../models/modelHeatmap/Heatmap');
 exports.updateHeatmap = async (req, res) => {
     const { section, action, x, y } = req.body;
     try {
+        // Buscar documento que coincida con la sección, acción y coordenadas
         let heatmap = await Heatmap.findOne({ section, action, x, y });
+
         if (heatmap) {
+            // Si existe, incrementar el contador
             heatmap.count += 1;
         } else {
-            heatmap = new Heatmap({ section, action, x, y });
+            // Si no existe, crear un nuevo documento
+            heatmap = new Heatmap({ section, action, x, y, count: 1 });
         }
+        
         await heatmap.save();
         res.status(200).json({ message: 'Heatmap updated', heatmap });
     } catch (error) {
@@ -17,6 +22,7 @@ exports.updateHeatmap = async (req, res) => {
         res.status(500).json({ message: 'Error updating heatmap', error });
     }
 };
+
 
 // Obtiene los datos del mapa de calor
 exports.getHeatmapData = async (req, res) => {
@@ -28,3 +34,22 @@ exports.getHeatmapData = async (req, res) => {
         res.status(500).json({ message: 'Error fetching heatmap data', error });
     }
 };
+
+// Controlador para obtener conteos por sección
+exports.getHeatmapCounts = async (req, res) => {
+    try {
+        const counts = await Heatmap.aggregate([
+            {
+                $group: {
+                    _id: { section: "$section", action: "$action" },
+                    totalCount: { $sum: "$count" }
+                }
+            }
+        ]);
+        res.status(200).json(counts);
+    } catch (error) {
+        console.error('Error fetching heatmap counts:', error);
+        res.status(500).json({ message: 'Error fetching heatmap counts', error });
+    }
+};
+
--- marketing/routes/routeHeatmap/heatmapRoutes.js
+++ marketing/routes/routeHeatmap/heatmapRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { updateHeatmap, getHeatmapData } = require('../../controllers/controllerHeatmap/heatmapController');
+const { updateHeatmap, getHeatmapData, getHeatmapCounts} = require('../../controllers/controllerHeatmap/heatmapController');
 const router = express.Router();
 
 router.post('/heatmap', updateHeatmap);
 
 router.get('/regresar', getHeatmapData);
+
+router.get('/conteo', getHeatmapCounts);  
 
 module.exports = router;
